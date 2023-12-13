c6ee53cad050dc03e67ab8c9d01de9920c6515d0
pkgajera <pointingr522@gmail.com>
2023-12-13 14:48:10 +0530

    added moment to h2

--- src/app/page.js
+++ src/app/page.js
@@ -41,6 +41,8 @@ export default function Home() {
           height={37}
           priority
         />
+        <br />
+        <h2>It is your time {moment().format("LTS")}</h2>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">

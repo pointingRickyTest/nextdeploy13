e1711867349ac8e510af78d2ac40e912a938e4eb
pkgajera <pointingr522@gmail.com>
2023-12-15 11:15:10 +0530

    backend api introduced

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
           priority
         />
         <br />
-        <h2>It is your time {moment().format("LTS")}</h2>
+        {/* <h2>It is your time {moment().format("LTS")}</h2> */}
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
